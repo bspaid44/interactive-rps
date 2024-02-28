64668df1655216fcc755d7301c2a98aa2d267845
bspaid44 <brandonspaid44@gmail.com>
2024-02-28 16:15:23 -0500

    Added comments to code blocks

--- javascript.js
+++ javascript.js
@@ -1,8 +1,10 @@
+// Creates global variables for elements we are updating on screen
 let playerScore = 0;
 let computerScore = 0;
 let drawScore = 0;
 let results = '';
 
+// Creates constant for the buttons defined in HTML ands add an event listener to trigger the function when clicked
 const rockButton = document.getElementById('rock');
 const paperButton = document.getElementById('paper');
 const scissorsButton = document.getElementById('scissors');
@@ -11,8 +13,17 @@ rockButton.addEventListener('click', () => playGameRock());
 paperButton.addEventListener('click', () => playGamePaper());
 scissorsButton.addEventListener('click', () => playGameScissors());
 
+// Defines choices for the computer to randomly select
 const choices = ['rock', 'paper', 'scissors'];
 
+// Randomly selects the computer choice
+let getComputerChoice = () => {
+    let randNum = Math.floor(Math.random() * choices.length);
+    
+    return choices[randNum];
+};
+
+// Updates the display for the score variables when called
 let updateScore = () => {
     const playerScorePara = document.getElementById('playerScore');
     const computerScorePara = document.getElementById('computerScore');
@@ -23,18 +34,14 @@ let updateScore = () => {
     drawScorePara.textContent = `Draws: ${drawScore}`;
 }
 
+// Updates the display for the results variable when called, will display whatever string passed into the function
 let updateResults = (phrase) => {
     const resultsPara = document.getElementById('results');
 
     resultsPara.textContent = phrase;
 }
 
-let getComputerChoice = () => {
-    let randNum = Math.floor(Math.random() * choices.length);
-    
-    return choices[randNum];
-};
-
+// Game Logic - Seperated into the three different game button events; Will increment the score variable by won if the case is true, and update the results and score displays for the user
 let playGameRock = () => {
     let computerChoice = getComputerChoice();
     let playerChoice = 'rock'
